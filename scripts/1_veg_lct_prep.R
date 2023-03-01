4c4cdbc530abeb66e00b3eb771ba22dd67bec52a
Andria Dawson <andria.dawson@gmail.com>
2023-02-28 17:10:50 -0700

    clean up and calibration model

--- scripts/1_veg_lct_prep.R
+++ scripts/1_veg_lct_prep.R
@@ -1,35 +1,54 @@
-# library(raster)
-library(maps)
-# library(reshape2)
+############################################################################################
+# 
+############################################################################################
+
+library(sp)
 library(dplyr)
 library(tidyr)
+library(elevatr)
 library(ggplot2)
-library(sp)
 
-veg_pred = readRDS('data/veg_pred_LC6k.RDS')
-
-
-breaks = c(-74, 0.1, 0.35, 0.7, seq(1.2, 11.7, by=0.5))
-slice_bins = seq(1, 25)
-slice_labels = c(50, 200, seq(500, 11500, by=500))
-
+############################################################################################
+# read in REVEALS reconstructions
+# define meta variables
+############################################################################################
 
 alb_proj = '+proj=aea +lat_1=50 +lat_2=70 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0
 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'
 
+ll_proj = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
+# ele_proj = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
+
+latlimits  <- c(10, 80) 
+longlimits <- c(-165, -50) 
+bounding_box = c(longlimits[1], latlimits[1], longlimits[2], latlimits[2])
+
+#veg_pred = readRDS('data/veg_pred_LC6k.RDS')
+veg_pred = readRDS('data/veg_pred_LGM_8.0.RDS')
+
 taxon2pft = read.csv('data/taxon2LCT_translation_v2.csv')
+
+# breaks = c(-74, 0.1, 0.35, 0.7, seq(1.2, 11.7, by=0.5))
+# slice_bins = seq(1, 25)
+# slice_labels = c(50, 200, seq(500, 11500, by=500))
+
+############################################################################################
+# 
+############################################################################################
+
 veg_pred$LCT = taxon2pft$LCT[match(veg_pred$taxon, tolower(taxon2pft$taxon))]
 
 veg_lct = aggregate(meansim ~ dataset_id + long + lat + ages + LCT, veg_pred, sum, na.rm=TRUE)
 
-k = veg_lct
+# k = veg_lct
 
 #assigning a crs to the pollen coordinates
-spdf <- SpatialPointsDataFrame(coords = k[,c('long', 'lat')], data = k,
-                               proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
+veg_lct_spatial = SpatialPointsDataFrame(coords = veg_lct[,c('long', 'lat')], 
+                                         data = veg_lct,
+                                         proj4string = CRS(ll_proj))
 
 #transforming to the albedo crs
-veg_transform = spTransform(spdf, alb_proj)
+veg_transform = spTransform(veg_lct_spatial, alb_proj)
 coords = coordinates(veg_transform)
 colnames(coords)[1] <- 'x'
 colnames(coords)[2] <- 'y'
@@ -41,20 +60,40 @@ pivot_mod = veg_lct %>%
 
 pivot_mod = data.frame(pivot_mod)
 
-library(elevatr)
+############################################################################################
+# 
+############################################################################################
 
-ele_proj = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
+# ele_proj = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
+# 
+# latLong = data.frame(pivot_mod[,c('long', 'lat')])
 
-latLong = data.frame(pivot_mod[,c('long', 'lat')])
+ele_get = get_elev_point(pivot_mod[,c('long', 'lat')], ll_proj, src = "aws")
 
-ele_get = get_elev_point(latLong, ele_proj, src = "aws")
+# ele_get = get_elev_point(latLong, ele_proj, src = "aws")
 
-lct_all = data.frame(pivot_mod[,c('ages', 'long', 'lat', 'x', 'y')], elev = ele_get$elevation, pivot_mod[,c('ET', 'OL', 'ST')])
-lct_all = lct_all[which(lct_all$long<(-40)),]
-lct_all = lct_all[which(lct_all$long>(-160)),]
-lct_all= lct_all[which(lct_all$lat<(73)),]
+lct_all = data.frame(pivot_mod[,c('ages', 'long', 'lat', 'x', 'y')], 
+                     elev = ele_get$elevation, 
+                     pivot_mod[,c('ET', 'OL', 'ST')])
+
+ggplot() + 
+  geom_point(data=lct_all, aes(x=long, y=lat), colour="blue") +
+  facet_wrap(~ages)
+
+lct_all = lct_all[which((lct_all$lat > latlimits[1])&
+                          (lct_all$lat < latlimits[2])&
+                          (lct_all$long > longlimits[1])&
+                          (lct_all$long < longlimits[2])), ]
+
+# lct_all = lct_all[which(lct_all$long<(-40)),]
+# lct_all = lct_all[which(lct_all$long>(-160)),]
+# lct_all= lct_all[which(lct_all$lat<(73)),]
 
 lct_all = lct_all[which(!((lct_all$lat<25)&(lct_all$long<(-150)))),]
+
+############################################################################################
+# 
+############################################################################################
 
 lct_modern = lct_all[which(lct_all$ages == 50), ]
 
@@ -65,10 +104,85 @@ saveRDS(lct_modern, 'data/lct_modern_reveals.RDS')
 ggplot() + 
   geom_point(data=lct_modern, aes(x=long, y=lat), colour="blue")
 
+############################################################################################
+# for paleo use 2000 year time bins
+############################################################################################
+
+# veg_pred = readRDS('data/veg_pred_LC6k.RDS')
+veg_pred = readRDS('data/veg_pred_LGM_8.0.RDS')
+
+taxon2pft = read.csv('data/taxon2LCT_translation_v2.csv')
+
+# breaks = c(-74, 0.1, 0.35, 0.7, seq(1.2, 11.7, by=0.5))
+# slice_bins = seq(1, 25)
+# slice_labels = c(50, 200, seq(500, 11500, by=500))
+
+############################################################################################
+# 
+############################################################################################
+
+veg_pred$LCT = taxon2pft$LCT[match(veg_pred$taxon, tolower(taxon2pft$taxon))]
+
+veg_lct = aggregate(meansim ~ dataset_id + long + lat + ages + LCT, veg_pred, sum, na.rm=TRUE)
+
+# k = veg_lct
+
+#assigning a crs to the pollen coordinates
+veg_lct_spatial = SpatialPointsDataFrame(coords = veg_lct[,c('long', 'lat')], 
+                                         data = veg_lct,
+                                         proj4string = CRS(ll_proj))
+
+#transforming to the albedo crs
+veg_transform = spTransform(veg_lct_spatial, alb_proj)
+coords = coordinates(veg_transform)
+colnames(coords)[1] <- 'x'
+colnames(coords)[2] <- 'y'
+
+veg_lct = data.frame(coords, veg_lct)
+
+pivot_mod = veg_lct %>%
+  pivot_wider(names_from = LCT, values_from = meansim)
+
+pivot_mod = data.frame(pivot_mod)
+
+############################################################################################
+# 
+############################################################################################
+
+# ele_proj = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
+# 
+# latLong = data.frame(pivot_mod[,c('long', 'lat')])
+
+ele_get = get_elev_point(pivot_mod[,c('long', 'lat')], ll_proj, src = "aws")
+
+# ele_get = get_elev_point(latLong, ele_proj, src = "aws")
+
+lct_all = data.frame(pivot_mod[,c('ages', 'long', 'lat', 'x', 'y')], 
+                     elev = ele_get$elevation, 
+                     pivot_mod[,c('ET', 'OL', 'ST')])
+
+ggplot() + 
+  geom_point(data=lct_all, aes(x=long, y=lat), colour="blue") +
+  facet_wrap(~ages)
+
+lct_all = lct_all[which((lct_all$lat > latlimits[1])&
+                          (lct_all$lat < latlimits[2])&
+                          (lct_all$long > longlimits[1])&
+                          (lct_all$long < longlimits[2])), ]
+
+# lct_all = lct_all[which(lct_all$long<(-40)),]
+# lct_all = lct_all[which(lct_all$long>(-160)),]
+# lct_all= lct_all[which(lct_all$lat<(73)),]
+
+lct_all = lct_all[which(!((lct_all$lat<25)&(lct_all$long<(-150)))),]
+
 
 lct_paleo = lct_all
+
+# lct_paleo = rbind(data.frame(ages=50, lct_modern), lct_paleo)
 saveRDS(lct_paleo, 'data/lct_paleo_reveals.RDS')
 
 ggplot() + 
   geom_point(data=lct_paleo, aes(x=long, y=lat), colour="blue") +
   facet_wrap(~ages)
+
---
+++ scripts/4_calibration_model.R
@@ -0,0 +1,439 @@
+library(gam)
+library(mgcv)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+
+alb_prod = "bluesky"
+
+cal_data =readRDS('data/calibration_modern_lct_bluesky.RDS')
+# cal_data =readRDS('data/lct_albedo_snow_modern_bluesky.RDS')
+
+cal_data2 = data.frame(site=seq(1,nrow(cal_data)), cal_data)
+cal_data3 = cal_data2[,c('site', 'ET', 'OL', 'ST')]
+
+###############################################################################################################
+## march calibration model
+###############################################################################################################
+
+ctrl <- list(nthreads=8)
+
+# # just x and y
+#works better with smaller k values 
+# was 900
+mod = mgcv::bam(bs03 ~ s(x, y, bs="gp", k=300),
+          data=cal_data, 
+          family=betar(link="logit"), 
+          method="REML", 
+          na.action=na.omit, 
+          control=ctrl)
+gam.check(mod)
+#vis.gam(mod,theta=30)
+
+
+# # just x and y
+mod1 = mgcv::bam(bs03 ~ s(x, y, bs="gp", k=400),
+          data=cal_data, 
+          family=betar(link="logit"), 
+          method="REML", 
+          na.action=na.omit, 
+          control=ctrl)
+gam.check(mod1)
+
+saveRDS(mod1, paste0('data/calibration_mod1_', alb_prod, '.RDS'))
+
+
+vis.gam(mod,theta=30)
+
+# add elevation
+#tp vs gp model
+mod2 = mgcv::bam(bs03 ~ s(x, y, bs='tp', k=400) + s(elev, k=30),
+          data=cal_data,
+          family=betar(link="logit"),
+          method="REML",
+          na.action=na.omit)
+
+# add eleveation
+#gp model works better than tp
+mod2 = mgcv::bam(bs03 ~ s(x, y, bs="gp", k=400) + s(elev, k=30),
+           data=cal_data, 
+           family=betar(link="logit"), 
+           method="REML", 
+           na.action=na.omit, 
+           control=ctrl)
+gam.check(mod2)
+#vis.gam(mod,theta=30)
+
+saveRDS(mod2, paste0('data/calibration_mod2_', alb_prod, '.RDS'))
+
+
+AIC(mod1, mod2)
+
+# add OL
+mod3 = mgcv::bam(bs03 ~ s(x, y, bs='gp', k=400) + s(elev, k=30) + s(OL, k=30),
+           data=cal_data, 
+           family=betar(link="logit"), 
+           method="REML", 
+           na.action=na.omit, 
+           control=ctrl)
+gam.check(mod3)
+
+saveRDS(mod3, paste0('data/calibration_mod3_', alb_prod, '.RDS'))
+
+
+AIC(mod, mod2, mod3)
+anova.gam(mod, mod2, mod3)
+
+# add ET
+mod4 = mgcv::bam(bs03 ~ s(x, y, bs='gp', k=400) + s(elev, k=30) + s(OL, k=30) + s(ET, k=30),
+           data=cal_data, 
+           family=betar(link="logit"), 
+           method="REML", 
+           na.action=na.omit, 
+           control=ctrl)
+gam.check(mod4)
+
+saveRDS(mod4, paste0('data/calibration_mod4_', alb_prod, '.RDS'))
+
+
+AIC(mod1, mod2, mod3, mod4)
+
+
+# add ST
+#this takes a while 
+# mod5 = mgcv::gam(bs03 ~ s(x, y, bs='gp', k=30) + s(elev, k=10) + s(OL, k=10) + s(ET, k=10) + s(ST, k=10),
+#            data=cal_data, 
+#            family=betar(link="logit"), 
+#            method="REML", 
+#            na.action=na.omit, 
+#            control=ctrl)
+mod5 = mgcv::bam(bs03 ~ s(x, y, bs='gp', k=400) + s(elev, k=30) + s(OL, k=30) + s(ET, k=30) + s(ST, k=30),
+                 data=cal_data, 
+                 family=betar(link="logit"), 
+                 method="REML", 
+                 na.action=na.omit, 
+                 control=ctrl)
+gam.check(mod5)
+
+saveRDS(mod5, paste0('data/calibration_mod5_', alb_prod, '.RDS'))
+
+AIC(mod1, mod2, mod3, mod4, mod5)
+
+mod6 = mgcv::bam(bs03 ~ s(x, y, bs='gp', k=400) + s(elev, k=30) + s(OL, ET, ST, k=30),
+                 data=cal_data, 
+                 family=betar(link="logit"), 
+                 method="REML", 
+                 na.action=na.omit, 
+                 control=ctrl)
+gam.check(mod6)
+
+saveRDS(mod6, paste0('data/calibration_mod6_', alb_prod, '.RDS'))
+
+AIC(mod1, mod2, mod3, mod4, mod5, mod6)
+
+###############################################################################################################
+## plotting model fit for bs03
+###############################################################################################################
+preds3 = predict.gam(mod3, 
+                     type="response", 
+                     newdata=cal_data,
+                     se.fit=TRUE)
+
+cal_data$preds_mod3 = preds3$fit
+#dat_all$predict = preds$fit
+
+ggplot(data=cal_data) + 
+  geom_point(aes(x=bs03, y=preds_mod3)) +
+  geom_abline(slope=1, intercept=0, colour="red") +
+  xlim(c(0,1)) + 
+  ylim(c(0,1))
+
+plot(mod3)
+plot(mod3, residuals=TRUE, all.terms=TRUE, pch=1)
+
+#model 4 
+preds = predict.gam(mod4, 
+                    type="response", 
+                    newdata=cal_data,
+                    se.fit=TRUE)
+
+cal_data$preds_mod4 = preds$fit
+#dat_all$predict = preds$fit
+
+ggplot(data=cal_data) + 
+  geom_point(aes(x=bs03, y=preds_mod4)) +
+  geom_abline(slope=1, intercept=0, colour="red") +
+  xlim(c(0,1)) + 
+  ylim(c(0,1))
+
+
+
+###############################################################################################################
+## march full model fit with x,y,OL,ET,ST,elevation
+###############################################################################################################
+
+cal_model = readRDS('data/calibration_model5.RDS')
+
+preds = predict.gam(cal_model, 
+                    type="response", 
+                    newdata=cal_data,
+                    se.fit=TRUE)
+
+cal_data$preds_mod5 = preds$fit
+#dat_all$predict = preds$fit
+
+ggplot(data=cal_data) + 
+  geom_point(aes(x=bs03, y=preds_mod5), size=2, alpha=0.5) +
+  geom_abline(slope=1, intercept=0, colour="red", size=1, lty=2) +
+  xlim(c(0,1)) + 
+  ylim(c(0,1)) +
+  coord_fixed() +  
+  theme_bw(18) +
+  theme(axis.title = element_text(size=14),
+        axis.text = element_text(size=14)) +
+  xlab('albedo (data)') +
+  ylab('albedo (model)')
+ggsave('figures/cal_model_vs_datamod5.png')
+ggsave('figures/cal_model_vs_datamod5.pdf')
+
+# cal_data$preds = cal_model$fitted.values
+cor(cal_data$bs03, cal_data$preds_mod5)
+summary(lm(preds_mod5~bs03, data=cal_data))
+################################################################################################
+#difference maps 
+#################################################################################################
+#calculating difference between model and predicted values from model #5
+#this is for march 
+cal_data$diff_mod5 = cal_data$preds_mod5 - cal_data$bs03
+saveRDS(cal_data, 'data/cal_data.RDS')
+######################################################################################################
+#FIGURES
+######################################################################################################
+pbs_ll = readRDS('data/map-data/geographic/pbs_ll.RDS')
+
+pbs = readRDS('data/map-data/geographic/pbs.RDS')
+
+
+#difference figure 
+ggplot()+
+  geom_polygon(data=pbs_ll, aes(long,lat, group = group), fill='grey') +
+  geom_point(data=cal_data, aes(x=long, y=lat, colour = diff_mod5))+
+  scale_colour_gradient2(low = 'green', high = 'pink', mid= 'yellow', limits = c(-0.2,0.2))+
+  ggtitle("Difference between predicted values and data")+
+  theme_bw()+
+  theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+ggsave("marchmod5.differencemap.png")
+
+
+#frequency of differences 
+#we can see that it is mainly focused at 0
+ggplot(data=cal_data) + 
+  geom_histogram(aes(x=diff_mod5), fill = "dark blue") +
+  labs( x="albedo differences", 
+       title="distribution of albedo differences using model 5" )+
+  theme_bw()+
+  theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+ggsave('figures/march_histo_frequency.png')
+
+
+
+###############################################################################################################
+## april calibration model
+###############################################################################################################
+
+ctrl <- list(nthreads=16)
+
+# # just x and y
+#works better with smaller k values 
+april_mod = gam(bs04 ~ s(x, y, bs="gp", k=900),
+          data=cal_data, 
+          family=betar(link="logit"), 
+          method="REML", 
+          na.action=na.omit, 
+          control=ctrl)
+gam.check(april_mod)
+#vis.gam(mod,theta=30)
+
+
+# # just x and y
+april_mod = gam(bs04 ~ s(x, y, bs="gp", k=175),
+          data=cal_data, 
+          family=betar(link="logit"), 
+          method="REML", 
+          na.action=na.omit, 
+          control=ctrl)
+gam.check(april_mod)
+
+vis.gam(april_mod,theta=30)
+
+# add elevation
+#tp vs gp model
+april_mod2 = gam(bs04 ~ s(x, y, bs='tp', k=900) + s(elev, k=30),
+           data=cal_data,
+           family=betar(link="logit"),
+           method="REML",
+           na.action=na.omit)
+
+# add eleveation
+#gp model works better than tp
+april_mod2 = gam(bs04 ~ s(x, y, bs="gp", k=100) + s(elev, k=10),
+           data=cal_data, 
+           family=betar(link="logit"), 
+           method="REML", 
+           na.action=na.omit, 
+           control=ctrl)
+gam.check(april_mod2)
+#vis.gam(mod,theta=30)
+
+AIC(april_mod, april_mod2)
+
+# add OL
+april_mod3 = gam(bs04 ~ s(x, y, bs='gp', k=100) + s(elev, k=10) + s(OL, k=10),
+           data=cal_data, 
+           family=betar(link="logit"), 
+           method="REML", 
+           na.action=na.omit, 
+           control=ctrl)
+gam.check(april_mod3)
+
+AIC(april_mod, april_mod2, april_mod3)
+anova.gam(april_mod, april_mod2, april_mod3)
+
+# add ET
+april_mod4 = gam(bs04 ~ s(x, y, bs='gp', k=100) + s(elev, k=10) + s(OL, k=10) + s(ET, k=10),
+           data=cal_data, 
+           family=betar(link="logit"), 
+           method="REML", 
+           na.action=na.omit, 
+           control=ctrl)
+gam.check(april_mod4)
+
+AIC(april_mod, april_mod2, april_mod3, april_mod4)
+
+
+# add ST
+#this takes a while 
+april_mod5 = gam(bs04 ~ s(x, y, bs='gp', k=500) + s(elev, k=10) + s(OL, k=10) + s(ET, k=10) + s(ST, k=5),
+           data=cal_data, 
+           family=betar(link="logit"), 
+           method="REML", 
+           na.action=na.omit, 
+           control=ctrl)
+gam.check(april_mod5)
+
+AIC(april_mod, april_mod2, april_mod3, april_mod4, april_mod5)
+
+
+saveRDS(april_mod, 'data/calibration_model1_april.RDS')
+
+saveRDS(april_mod5, 'data/calibration_model5_april.RDS')
+
+###############################################################################################################
+## plotting model fit for bs03
+###############################################################################################################
+preds3 = predict.gam(mod3, 
+                     type="response", 
+                     newdata=cal_data,
+                     se.fit=TRUE)
+
+cal_data$preds_mod3 = preds3$fit
+#dat_all$predict = preds$fit
+
+ggplot(data=cal_data) + 
+  geom_point(aes(x=bs03, y=preds_mod3)) +
+  geom_abline(slope=1, intercept=0, colour="red") +
+  xlim(c(0,1)) + 
+  ylim(c(0,1))
+
+plot(mod3)
+plot(mod3, residuals=TRUE, all.terms=TRUE, pch=1)
+
+#model 4 
+preds = predict.gam(mod4, 
+                    type="response", 
+                    newdata=cal_data,
+                    se.fit=TRUE)
+
+cal_data$preds_mod4 = preds$fit
+#dat_all$predict = preds$fit
+
+ggplot(data=cal_data) + 
+  geom_point(aes(x=bs03, y=preds_mod4)) +
+  geom_abline(slope=1, intercept=0, colour="red") +
+  xlim(c(0,1)) + 
+  ylim(c(0,1))
+
+
+
+###############################################################################################################
+## march full model fit with x,y,OL,ET,ST,elevation
+###############################################################################################################
+
+cal_model = readRDS('data/calibration_model5.RDS')
+
+preds = predict.gam(cal_model, 
+                    type="response", 
+                    newdata=cal_data,
+                    se.fit=TRUE)
+
+cal_data$preds_mod5 = preds$fit
+#dat_all$predict = preds$fit
+
+ggplot(data=cal_data) + 
+  geom_point(aes(x=bs03, y=preds_mod5)) +
+  geom_abline(slope=1, intercept=0, colour="red", size=1, lty=2) +
+  xlim(c(0,1)) + 
+  ylim(c(0,1)) +
+  coord_fixed() +  
+  theme_bw() +
+  theme(axis.title = element_text(size=14),
+        axis.text = element_text(size=14)) +
+  xlab('Albedo Data') +
+  ylab('Albedo Model')
+ggsave('figures/cal_model_vs_datamod5.png')
+#ggsave('figures/cal_model_vs_data.pdf')
+
+cal_data$preds = cal_model$fitted.values
+
+################################################################################################
+#difference maps 
+#################################################################################################
+#calculating difference between model and predicted values from model #5
+#this is for march 
+cal_data$diff_mod5 = cal_data$preds_mod5 - cal_data$bs03
+saveRDS(cal_data, 'data/cal_data.RDS')
+######################################################################################################
+#FIGURES
+######################################################################################################
+pbs_ll = readRDS('data/map-data/geographic/pbs_ll.RDS')
+
+pbs = readRDS('data/map-data/geographic/pbs.RDS')
+
+
+#difference figure 
+ggplot()+
+  geom_polygon(data=pbs_ll, aes(long,lat, group = group), fill='grey') +
+  geom_point(data=cal_data, aes(x=long, y=lat, colour = diff_mod5))+
+  scale_colour_gradient2(low = 'green', high = 'pink', mid= 'yellow', limits = c(-0.2,0.2))+
+  ggtitle("Difference between predicted values and data")+
+  theme_bw()+
+  theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+ggsave("marchmod5.differencemap.png")
+
+
+#frequency of differences 
+#we can see that it is mainly focused at 0
+ggplot(data=cal_data) + 
+  geom_histogram(aes(x=diff_mod5), fill = "dark blue") +
+  labs( x="albedo differences", 
+        title="distribution of albedo differences using model 5" )+
+  theme_bw()+
+  theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+ggsave('figures/march_histo_frequency.png')
+
+
